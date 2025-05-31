97035244de42b529b102278392b1d85e79bb4314
Mark Rittman <mark.rittman@rittmananalytics.com>
2025-05-31 22:54:06 +0100

    Add Dockerfile and update Cloud Run deployment docs

--- cloud-function/config/config.py
+++ cloud-function/config/config.py
@@ -1,6 +1,4 @@
 import os
-import json
-import base64
 from google.cloud import secretmanager
 
 
@@ -21,30 +19,6 @@ version_id = "latest"
 OPENAI_API_KEY = access_secret_version(project_id, secret_id, version_id)
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-
-
-# Function to access secret from Google Secret Manager
-def access_secret_version(project_id, secret_id, version_id):
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(request={"name": name})
-    secret_value = response.payload.data.decode("UTF-8")
-    return secret_value
-
-# Function to encode secret value from JSON to base64
-def encode_to_base64(secret_value):
-    encoded_value = base64.b64encode(secret_value.encode("utf-8")).decode("utf-8")
-    return encoded_value
-
-# Set secrets manager variables
-project_id = "ra-development"
-secret_id = "ra-langchain-service-account"
-version_id = "latest"
-
-# Set base64 encoded environment variable gcp_credentials
-secret_value_gcp_service_account = access_secret_version(project_id, secret_id, version_id)
-GCP_CREDENTIALS = encode_to_base64(secret_value_gcp_service_account)
-os.environ["GCP_CREDENTIALS"] = GCP_CREDENTIALS
 
 # Set openai model variable
 OPEN_AI_MODEL="gpt-4-turbo"
